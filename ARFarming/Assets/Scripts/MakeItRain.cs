a964f3f34d2edab3deef04c8e1465cea2b650268
jddeep <jddeep003@gmail.com>
2019-10-20 17:10:28 +0530

    added AR module

---
+++ ARFarming/Assets/Scripts/MakeItRain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeItRain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleRain()
+    {
+        print("Active?"+gameObject.activeSelf);
+        if (gameObject.activeSelf)
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
+    }
+
+}
---
+++ ARFarming/Assets/Scripts/Swapper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swapper : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ExecuteAfterTime(100));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+        SwapPlantsWithFruits();
+       
+
+    }
+
+    private void SwapPlantsWithFruits()
+    {
+        GameObject[] plants = GameObject.FindGameObjectsWithTag("Plants");
+        GameObject[] fruits = GameObject.FindGameObjectsWithTag("Fruits");
+        for(int i = 0; i<fruits.Length; i++)
+        {
+            fruits[i].SetActive(true);
+            fruits[i].transform.position = new Vector3(plants[i].transform.position.x,plants[i].transform.position.y + 0.3f,plants[i].transform.position.z);
+            fruits[i].transform.rotation = Quaternion.identity;
+
+            Destroy(plants[i]);
+        }
+    }
+
+    private void SwapFruitsWithPlants()
+    {
+        //GameObject[] plants = GameObject.FindGameObjectsWithTag("Plants");
+        //GameObject[] fruits = GameObject.FindGameObjectsWithTag("Fruits");
+        //for (int i = 0; i < plants.Length; i++)
+        //{
+        //    Instantiate(plants[i], fruits[i].transform);
+        //    Destroy(fruits[i]);
+        //}
+
+    }
+}
